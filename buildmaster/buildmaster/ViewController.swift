c404ae7fad087b136f3b385e79b6124e16e210f0
Christian Isaacs <christian.isaacs@alaskaair.com>
2018-08-23 17:17:35 -0700

    Setting up Project and App Center

---
+++ buildmaster/buildmaster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  buildmaster
+//
+//  Created by Christian on 8/23/18.
+//  Copyright © 2018 Christian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
